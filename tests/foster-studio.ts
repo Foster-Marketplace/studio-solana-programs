97e7557bd40eba0ab892d4567517e604d2a8cba9
Kartik Soneji <kartiksoneji@rocketmail.com>
2024-12-02 16:26:58 +0530

    Add linked edition claim tests

--- tests/foster-studio.ts
+++ tests/foster-studio.ts
@@ -9,7 +9,13 @@ import * as assert from "assert";
 import { Keypair, LAMPORTS_PER_SOL } from "./web3";
 import { admin, user, DEFAULT_PRODUCT_CONFIG, mint } from "./constants";
 import setupProgram from "./setup";
-import { buyProduct, buyProductBuilder, createProduct, studio } from "./lib";
+import {
+  buyProduct,
+  buyProductBuilder,
+  createProduct,
+  findClaimMarkerPda,
+  studio,
+} from "./lib";
 import {
   deepStrictEqual,
   getBalanceDelta,
@@ -18,6 +24,7 @@ import {
   sleep,
   unixTimestamp,
 } from "./utils";
+import { createAndMintNft, createPrint } from "./mplTokenMetadata";
 
 describe("foster-studio", () => {
   const provider = anchor.AnchorProvider.env();
@@ -229,6 +236,205 @@ describe("foster-studio", () => {
     deepStrictEqual(buyError.error, {
       errorCode: { code: "NoMoreSupply", number: 6004 },
       errorMessage: "No more supply",
+      comparedValues: undefined,
+      origin: undefined,
+    });
+  });
+
+  it("buy product with linked master edition", async () => {
+    const { nftMint } = await createAndMintNft({
+      connection,
+      payer: admin,
+
+      name: "Master Edition NFT",
+      symbol: "TEST",
+      uri: "https://example.com/",
+      maxSupply: 1,
+    });
+
+    const { productId } = await createProduct({
+      overrides: {
+        linkedMasterNft: nftMint,
+        maxSupply: { followMasterEdition: {} },
+      },
+    });
+
+    const { editionMint } = await createPrint({
+      connection,
+      payer: admin,
+      owner: user.publicKey,
+      masterEditionMint: nftMint,
+      editionNumber: 1,
+    });
+
+    const buySignature = await buyProduct({
+      productId,
+      buyer: user,
+      edition: editionMint,
+    });
+    await sleep(2000);
+
+    // less than max supply works
+    const buyTx = await connection.getParsedTransaction(
+      buySignature,
+      "confirmed"
+    );
+    assert.equal(buyTx.meta.err, null);
+
+    const product = await studio.account.merchProduct.fetch(productId);
+    deepStrictEqual(product, {
+      ...DEFAULT_PRODUCT_CONFIG,
+      id: productId,
+      currentSupply: "1",
+      linkedMasterNft: nftMint,
+      maxSupply: {
+        followMasterEdition: {},
+      },
+      claimsPerEdition: 0,
+    });
+
+    const claimMarkerAccount = await connection.getAccountInfo(
+      findClaimMarkerPda(editionMint)
+    );
+    deepStrictEqual(claimMarkerAccount.data.toString("hex"), "01000000");
+
+    // attempting to buy without edition fails
+    const noEditionBuyError = await invertPromise<AnchorError>(
+      buyProduct({
+        productId,
+        buyer: user,
+      })
+    );
+
+    deepStrictEqual(noEditionBuyError.error, {
+      errorCode: { code: "MissingEdition", number: 6006 },
+      errorMessage: "Missing edition",
+      comparedValues: undefined,
+      origin: undefined,
+    });
+
+    // attempting to exceed supply fails
+    const exceededSupplyBuyError = await invertPromise<AnchorError>(
+      buyProduct({
+        productId,
+        buyer: user,
+        edition: editionMint,
+      })
+    );
+
+    deepStrictEqual(exceededSupplyBuyError.error, {
+      errorCode: { code: "NoMoreSupply", number: 6004 },
+      errorMessage: "No more supply",
+      comparedValues: undefined,
+      origin: undefined,
+    });
+  });
+
+  it("buy product with linked master edition and multiple claims", async () => {
+    const { nftMint } = await createAndMintNft({
+      connection,
+      payer: admin,
+
+      name: "Master Edition NFT",
+      symbol: "TEST",
+      uri: "https://example.com/",
+      maxSupply: 1,
+    });
+
+    const { productId } = await createProduct({
+      overrides: {
+        linkedMasterNft: nftMint,
+        maxSupply: { some: [new anchor.BN(3)] },
+        claimsPerEdition: 2,
+      },
+    });
+
+    const { editionMint } = await createPrint({
+      connection,
+      payer: admin,
+      owner: user.publicKey,
+      masterEditionMint: nftMint,
+      editionNumber: 1,
+    });
+
+    const buySignature = await buyProduct({
+      productId,
+      buyer: user,
+      edition: editionMint,
+    });
+    await sleep(2000);
+
+    // less than max supply works
+    const buyTx = await connection.getParsedTransaction(
+      buySignature,
+      "confirmed"
+    );
+    assert.equal(buyTx.meta.err, null);
+
+    let product = await studio.account.merchProduct.fetch(productId);
+    deepStrictEqual(product, {
+      ...DEFAULT_PRODUCT_CONFIG,
+      id: productId,
+      currentSupply: "1",
+      linkedMasterNft: nftMint,
+      maxSupply: {
+        some: {
+          "0": "3",
+        },
+      },
+      claimsPerEdition: 2,
+    });
+
+    let claimMarkerAccount = await connection.getAccountInfo(
+      findClaimMarkerPda(editionMint)
+    );
+    deepStrictEqual(claimMarkerAccount.data.toString("hex"), "01000000");
+
+    // multiple claims with same edition works
+    const secondClaimSignature = await buyProduct({
+      productId,
+      buyer: user,
+      edition: editionMint,
+    });
+    await sleep(2000);
+
+    const secondClaimTx = await connection.getParsedTransaction(
+      secondClaimSignature,
+      "confirmed"
+    );
+    assert.equal(secondClaimTx.meta.err, null);
+
+    product = await studio.account.merchProduct.fetch(productId);
+    deepStrictEqual(product, {
+      ...DEFAULT_PRODUCT_CONFIG,
+      id: productId,
+      currentSupply: "2",
+      linkedMasterNft: nftMint,
+      maxSupply: {
+        some: {
+          "0": "3",
+        },
+      },
+      claimsPerEdition: 2,
+    });
+
+    claimMarkerAccount = await connection.getAccountInfo(
+      findClaimMarkerPda(editionMint)
+    );
+    deepStrictEqual(claimMarkerAccount.data.toString("hex"), "02000000");
+
+    // attempting to exceed claim allotment fails
+    const exceededClaimsBuyError = await invertPromise<AnchorError>(
+      buyProduct({
+        productId,
+        buyer: user,
+        edition: editionMint,
+      })
+    );
+
+    deepStrictEqual(exceededClaimsBuyError.error, {
+      errorCode: { code: "NoMoreClaims", number: 6008 },
+      errorMessage: "No more claims left on edition",
       comparedValues: undefined,
       origin: undefined,
     });
--- tests/mplTokenMetadata.ts
+++ tests/mplTokenMetadata.ts
@@ -1,17 +1,118 @@
-import { PublicKey } from "./web3";
+import { Connection, Keypair, PublicKey } from "./web3";
+import {
+  signerIdentity,
+  percentAmount,
+  createSignerFromKeypair,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createAndMint,
+  mplTokenMetadata,
+  TokenStandard,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
+  findMasterEditionPda as findMasterEditionPdaUmi,
+  printSupply,
+  printV2,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  fromWeb3JsPublicKey,
+  toWeb3JsPublicKey,
+  fromWeb3JsKeypair,
+} from "@metaplex-foundation/umi-web3js-adapters";
 
 export const MPL_TOKEN_METADATA_ID = new PublicKey(
-  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  MPL_TOKEN_METADATA_PROGRAM_ID
 );
 
 export function findMasterEditionPda(mint: PublicKey): PublicKey {
-  return PublicKey.findProgramAddressSync(
-    [
-      Buffer.from("metadata"),
-      MPL_TOKEN_METADATA_ID.toBuffer(),
-      mint.toBuffer(),
-      Buffer.from("edition"),
-    ],
-    MPL_TOKEN_METADATA_ID
-  )[0];
+  const umi = createUmi("http://localhost");
+  return toWeb3JsPublicKey(
+    findMasterEditionPdaUmi(umi, {
+      mint: fromWeb3JsPublicKey(mint),
+    })[0]
+  );
+}
+
+export interface CreateAndMintNftArgs {
+  connection: Connection;
+  payer: Keypair;
+  owner?: PublicKey;
+
+  mint?: Keypair;
+  name: string;
+  symbol: string;
+  uri: string;
+
+  maxSupply: number;
+}
+export async function createAndMintNft({
+  connection,
+  payer,
+  owner = payer.publicKey,
+  mint = Keypair.generate(),
+  name,
+  symbol,
+  uri,
+  maxSupply,
+}: CreateAndMintNftArgs) {
+  const umi = createUmi(connection);
+  const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(payer));
+  umi.use(signerIdentity(signer)).use(mplTokenMetadata());
+
+  const signature = await createAndMint(umi, {
+    mint: createSignerFromKeypair(umi, fromWeb3JsKeypair(mint)),
+    tokenOwner: fromWeb3JsPublicKey(owner),
+    tokenStandard: TokenStandard.NonFungible,
+
+    name,
+    symbol,
+    uri,
+    sellerFeeBasisPoints: percentAmount(0),
+
+    printSupply:
+      maxSupply === undefined
+        ? printSupply("Zero")
+        : maxSupply === null
+        ? printSupply("Unlimited")
+        : printSupply("Limited", [maxSupply]),
+  }).sendAndConfirm(umi);
+
+  return {
+    nftMint: mint.publicKey,
+    signature,
+  };
+}
+
+export interface CreatePrintArgs {
+  connection: Connection;
+  payer: Keypair;
+  owner?: PublicKey;
+  masterEditionMint: PublicKey;
+  editionMint?: Keypair;
+  editionNumber: number;
+}
+export async function createPrint({
+  connection,
+  payer,
+  owner = payer.publicKey,
+  masterEditionMint,
+  editionMint = Keypair.generate(),
+  editionNumber,
+}: CreatePrintArgs) {
+  const umi = createUmi(connection);
+  const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(payer));
+  umi.use(signerIdentity(signer)).use(mplTokenMetadata());
+
+  const signature = await printV2(umi, {
+    masterEditionMint: fromWeb3JsPublicKey(masterEditionMint),
+    tokenStandard: TokenStandard.NonFungibleEdition,
+    editionTokenAccountOwner: fromWeb3JsPublicKey(owner),
+    editionMint: createSignerFromKeypair(umi, fromWeb3JsKeypair(editionMint)),
+    editionNumber,
+  }).sendAndConfirm(umi);
+
+  return {
+    editionMint: editionMint.publicKey,
+    signature,
+  };
 }
